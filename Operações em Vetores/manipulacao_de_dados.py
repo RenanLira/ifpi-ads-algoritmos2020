415c66088f0560c1666ca4116b9ded78a0d4f53d
RenanLira <48999721+RenanLira@users.noreply.github.com>
2020-09-29 18:25:45 -0300

    adicionado novas funções

--- Operações em Vetores/manipulacao_de_dados.py
+++ Operações em Vetores/manipulacao_de_dados.py
@@ -6,6 +6,9 @@ def main():
     menu += '\n 4 - Remover do Final'
     menu += '\n 5 - Remover do Início'
     menu += '\n 6 - Remover de Posição Específica'
+    menu += '\n 7 - Quantidade de numeros pares'
+    menu += '\n 8 - Quantidade de numeros impares'
+    menu += '\n 9 - Quantidade de numeros Primos'
     menu += '\n 0 - Sair '
     menu += '\n\n Opção >>> '
 
@@ -35,37 +38,77 @@ def main():
                         buscar_posicao(lista, posicao)
 
                     elif opcao == 6:
-                        continuar = input('Deseja continuar com a exclusão? (S/N): ').upper()
-                        if continuar == 'S':
-                            remover(lista, lista[posicao])
-                        elif continuar == 'N':
-                            mostrar_msg('', 2)
-                        else:
-                            mostrar_msg('Opção invalida...', 2)
-            
+                        remover(lista, lista[posicao])
 
             elif opcao == 3:
                 mostrar_lista(lista)
 
-            elif opcao == 4 or opcao == 5:
-                continuar = input('Deseja continuar com a exclusão? (S/N): ').upper()
+            elif opcao == 4:
+                remover(lista, lista[len(lista)-1])
 
-                if continuar == 'S':
-                    if opcao == 4:
-                        remover(lista, lista[len(lista)-1])
-                    elif opcao == 5:
-                        remover(lista, lista[0])
+            elif opcao == 5:
+                remover(lista, lista[0])
 
-                elif continuar == 'N':
-                    mostrar_msg('', 2)
-                else:
-                    mostrar_msg('Opção invalida...', 2)
-            
-                
+            elif opcao == 7:
+                total_numeros(lista, 'par')
+
+            elif opcao == 8:
+                total_numeros(lista, 'impar')
+
+            elif opcao == 9:
+                numeros_primos(lista)
+
+
+def total_numeros(lista, tipo):
+    pares = 0
+    impares = 0
+    for i in range(len(lista)):
+        if lista[i] % 2 == 0:
+            pares += 1
+            if tipo == 'par':
+                print(f'{i} = {lista[i]}')
+
+        elif lista[i] % 2 != 0:
+            impares += 1
+            if tipo == 'impar':
+                print(f'{i} = {lista[i]}')
+    
+
+    if tipo == 'par':
+        mostrar_msg(f'Há um total de {pares} numeros pares')
+
+    elif tipo == 'impar':
+        mostrar_msg(f'Há um total de {impares} numeros impares')
+
+
+def numeros_primos(lista):
+    total_primos = 0
+    for i in range(len(lista)):
+        contador = 0
+        for j in range(1, lista[i]+1):
+            if lista[i] % j == 0:
+                contador += 1
+
+        if contador == 2:
+            total_primos += 1
+            print(f'{i} = {lista[i]}')
+
+    mostrar_msg(f'Há um total de {total_primos} numeros primos')
+
+
 
 def remover(lista, num):
-    lista.remove(num)
-    mostrar_msg('Item excluido com sucesso...')
+    continuar = input('Deseja continuar com a exclusão? (S/N): ').upper()
+
+    if continuar == 'S':
+        lista.remove(num)
+        mostrar_msg('Item excluido com sucesso...')
+
+    elif continuar == 'N':
+        mostrar_msg('', 2)
+    
+    else:
+        mostrar_msg('Opção Invalida', 2)
 
 
 def mostrar_lista(lista):
@@ -84,17 +127,17 @@ def buscar_posicao(lista, posicao):
 
 def inserir_valores(lista, qtd):
     for i in range(qtd):
-        num = float(input(f'Valor {i+1}: '))
+        num = int(input(f'Valor {i+1}: '))
         lista.append(num)
 
 
     mostrar_msg('Valores adicionados com sucesso...')
 
 
-def mostrar_msg(msg, modo=1):
+def mostrar_msg(msg, tipo=1):
     print(msg)
 
-    if modo == 1:
+    if tipo == 1:
         final = 'continuar'
     else:
         final = 'voltar'
