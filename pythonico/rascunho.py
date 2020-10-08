c515ec62ca568ccb4c8fa8d7c46c4b712add2c41
RenanLira <48999721+RenanLira@users.noreply.github.com>
2020-10-08 01:09:14 -0300

    new file

---
+++ pythonico/rascunho.py
@@ -0,0 +1,23 @@
+import os
+
+def main():
+    condicao = True
+    while condicao:
+        try:
+            numeros = list(map(lambda a: int(a)*2 if a else '', input('->(deixe vazio para cancelar) ').split(' ')))
+            
+            break
+        except:
+            os.system('cls')
+            print('digite numeros por favor')
+    
+    
+    print(numeros)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
