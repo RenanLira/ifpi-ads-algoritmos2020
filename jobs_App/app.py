fbf42f30dfceb38824c71e5e0ef840143acbe355
RenanLira <48999721+RenanLira@users.noreply.github.com>
2020-10-22 21:47:39 -0300

    .

---
+++ jobs_App/app.py
@@ -0,0 +1,23 @@
+import time
+
+import json
+import os
+
+
+def main():
+    construir_menu('a', 'b', 'c')
+    input()
+    while opcao() != 0:
+        pass
+    
+
+def construir_menu(*lista):
+    print(lista)
+
+
+def opcao():
+    pass
+
+
+
+main()
